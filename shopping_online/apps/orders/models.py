5ee54772e5014ea3dc479c9c5664b96ffa13cb52
mrcxrzs1314 <1665300581@qq.com>
2019-05-22 18:05:05 +0800

    补充

--- shopping_online/apps/orders/models.py
+++ shopping_online/apps/orders/models.py
@@ -6,4 +6,6 @@ class orders(models.Model):
     u_name = models.CharField('用户名', max_length=20)
     image = models.CharField('图片',max_length=500)
     s_price = models.SmallIntegerField('总价')
-    status = models.SmallIntegerField('状态')
+    status = models.CharField('状态',max_length=6)
+    address = models.CharField('地址',max_length=255)
+    phone = models.CharField('手机号',max_length=11)
